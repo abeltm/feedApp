c1f48e9f7c2c5ba62b70fc4fccb7b87e6a0b0738
Abel <abeltm893@gmail.com>
2024-07-25 13:18:27 -0400

    added ResourceProvider which provide access to config parameters in the Spring application, and YamlPropertySourceFactory which reads YAML file and create object names Properties which are defined in the config.yml file

---
+++ src/main/java/com/bptn/feedapp/provider/ResourceProvider.java
@@ -0,0 +1,5 @@
+package com.bptn.feedapp.provider;
+
+public class ResourceProvider {
+
+}
---
+++ src/main/java/com/bptn/feedapp/provider/factory/YamlPropertySourceFactory.java
@@ -0,0 +1,21 @@
+package com.bptn.feedapp.provider.factory;
+
+import java.io.IOException;
+
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertySourceFactory;
+
+public class YamlPropertySourceFactory implements PropertySourceFactory {
+
+	public YamlPropertySourceFactory() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public PropertySource<?> createPropertySource(String name, EncodedResource resource) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
