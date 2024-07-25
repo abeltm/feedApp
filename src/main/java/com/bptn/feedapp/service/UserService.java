e4c7bca9dafc12b1588017d0f8f3412a212b7fec
Abel <abeltm893@gmail.com>
2024-07-25 15:15:53 -0400

    added emailSevice instance variable and sendVerificationEmail() method under signup() method

--- src/main/java/com/bptn/feedapp/service/UserService.java
+++ src/main/java/com/bptn/feedapp/service/UserService.java
@@ -16,6 +16,9 @@ public class UserService {
 
 	@Autowired
 	UserRepository userRepository;
+	    
+	@Autowired
+	EmailService emailService;
 
 	public List<User> listUsers() {
 		return this.userRepository.findAll();
@@ -39,6 +42,8 @@ public class UserService {
 		user.setCreatedOn(Timestamp.from(Instant.now()));
 		
 		this.userRepository.save(user);
+	    
+		this.emailService.sendVerificationEmail(user);
 		
 		return user;
 	}
