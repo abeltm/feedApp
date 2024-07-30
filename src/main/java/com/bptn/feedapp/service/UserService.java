1c111e9701bcc72035999929de0718dc83af4cfc
Abel <abeltm893@gmail.com>
2024-07-30 13:51:16 -0400

    updated UserService class by adding updateUser() method in userService class that ipdates user details

--- src/main/java/com/bptn/feedapp/service/UserService.java
+++ src/main/java/com/bptn/feedapp/service/UserService.java
@@ -6,6 +6,8 @@ import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.bptn.feedapp.exception.domain.EmailExistException;
 import com.bptn.feedapp.exception.domain.EmailNotVerifiedException;
@@ -30,7 +33,7 @@ import org.slf4j.Logger;
 
 @Service
 public class UserService {
-	
+
 	final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
@@ -44,13 +47,12 @@ public class UserService {
 
 	@Autowired
 	AuthenticationManager authenticationManager;
-	
+
 	@Autowired
 	JwtService jwtService;
 
 	@Autowired
 	ResourceProvider provider;
-	
 
 	public List<User> listUsers() {
 		return this.userRepository.findAll();
@@ -118,7 +120,7 @@ public class UserService {
 	private Authentication authenticate(String username, String password) {
 		return this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
 	}
-	
+
 	public User authenticate(User user) {
 
 		/* Spring Security Authentication. */
@@ -130,11 +132,11 @@ public class UserService {
 
 	public HttpHeaders generateJwtHeader(String username) {
 		HttpHeaders headers = new HttpHeaders();
-		headers.add(AUTHORIZATION, this.jwtService.generateJwtToken(username,this.provider.getJwtExpiration()));
+		headers.add(AUTHORIZATION, this.jwtService.generateJwtToken(username, this.provider.getJwtExpiration()));
 
 		return headers;
 	}
-	
+
 	public void sendResetPasswordEmail(String emailId) {
 
 		Optional<User> opt = this.userRepository.findByEmailId(emailId);
@@ -145,25 +147,65 @@ public class UserService {
 			logger.debug("Email doesn't exist, {}", emailId);
 		}
 	}
-	
+
 	public void resetPassword(String password) {
 
 		String username = SecurityContextHolder.getContext().getAuthentication().getName();
 
 		User user = this.userRepository.findByUsername(username)
-					.orElseThrow(() -> new UserNotFoundException(String.format("Username doesn't exist, %s",username)));
+				.orElseThrow(() -> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
 
 		user.setPassword(this.passwordEncoder.encode(password));
 
 		this.userRepository.save(user);
 	}
-	
+
 	public User getUser() {
 
 		String username = SecurityContextHolder.getContext().getAuthentication().getName();
 
 		/* Get User from the DB. */
 		return this.userRepository.findByUsername(username)
-	.orElseThrow(() -> new UserNotFoundException(String.format("Username doesn't exist, %s",username)));
+				.orElseThrow(() -> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
 	}
+
+	private void updateValue(Supplier<String> getter, Consumer<String> setter) {
+
+		Optional.ofNullable(getter.get())
+				// .filter(StringUtils::hasText)
+				.map(String::trim).ifPresent(setter);
+	}
+
+	private void updatePassword(Supplier<String> getter, Consumer<String> setter) {
+
+		Optional.ofNullable(getter.get()).filter(StringUtils::hasText).map(this.passwordEncoder::encode)
+				.ifPresent(setter);
+	}
+
+	private User updateUser(User user, User currentUser) {
+
+		this.updateValue(user::getFirstName, currentUser::setFirstName);
+		this.updateValue(user::getLastName, currentUser::setLastName);
+		this.updateValue(user::getPhone, currentUser::setPhone);
+		this.updateValue(user::getEmailId, currentUser::setEmailId);
+		this.updatePassword(user::getPassword, currentUser::setPassword);
+
+		return this.userRepository.save(currentUser);
+	}
+	
+	public User updateUser(User user) {
+		
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+		/* Validates the new email if provided */
+		this.userRepository.findByEmailId(user.getEmailId())
+	                            .filter(u->!u.getUsername().equals(username))
+	                            .ifPresent(u -> {throw new EmailExistException(String.format("Email already exists, %s", u.getEmailId()));});
+		    
+		/* Get and Update User */	
+		return this.userRepository.findByUsername(username)
+					            .map(currentUser -> this.updateUser(user, currentUser))
+					            .orElseThrow(()-> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
+	}
+
 }
