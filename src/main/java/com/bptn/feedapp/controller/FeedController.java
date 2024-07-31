f24c71a125ae7cc2b04b9884a1a3aabceaa17c66
Abel <abeltm893@gmail.com>
2024-07-31 14:02:53 -0400

    updated FeedController by adding getFeed() which retrieves a feed and return a response

--- src/main/java/com/bptn/feedapp/controller/FeedController.java
+++ src/main/java/com/bptn/feedapp/controller/FeedController.java
@@ -1,18 +1,18 @@
 package com.bptn.feedapp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import com.bptn.feedapp.service.FeedService;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import com.bptn.feedapp.jpa.Feed;
+import com.bptn.feedapp.service.FeedService;
 
 @CrossOrigin
 @RestController
@@ -30,6 +30,14 @@ public class FeedController {
 		logger.debug("Creating Feed");
 				
 		return this.feedService.createFeed(feed);
+	}
+	
+	@GetMapping("/{feedId}")
+	public Feed getFeed(@PathVariable int feedId) {
+			
+		logger.debug("Getting Feed, feedId: {}", feedId);
+			
+		return this.feedService.getFeedById(feedId);	
 	}
 	
 }
