b7c3d19ac6be8cd6f341eac7cae9073914f2d430
Abel <abeltm893@gmail.com>
2024-07-31 14:34:10 -0400

    updated FeedService by adding getUserFeeds() method that returns pageable response of feeds for authenticated users

--- src/main/java/com/bptn/feedapp/service/FeedService.java
+++ src/main/java/com/bptn/feedapp/service/FeedService.java
@@ -16,34 +16,52 @@ import com.bptn.feedapp.jpa.User;
 import com.bptn.feedapp.repository.FeedRepository;
 import com.bptn.feedapp.repository.UserRepository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import com.bptn.feedapp.domain.PageResponse;
+
 @Service
 public class FeedService {
-	
+
 	final Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
 	UserRepository userRepository;
 
 	@Autowired
 	FeedRepository feedRepository;
-	
+
 	public Feed createFeed(Feed feed) {
 
 		String username = SecurityContextHolder.getContext().getAuthentication().getName();
 
 		User user = this.userRepository.findByUsername(username)
-					.orElseThrow(() -> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
+				.orElseThrow(() -> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
 
 		feed.setUser(user);
 		feed.setCreatedOn(Timestamp.from(Instant.now()));
 
 		return this.feedRepository.save(feed);
 	}
-	
+
 	public Feed getFeedById(int feedId) {
 
 		return this.feedRepository.findById(feedId)
-		.orElseThrow(() -> new FeedNotFoundException(String.format("Feed doesn't exist, %d", feedId)));
+				.orElseThrow(() -> new FeedNotFoundException(String.format("Feed doesn't exist, %d", feedId)));
 	}
-	
+
+	public PageResponse<Feed> getUserFeeds(int pageNum, int pageSize) {
+
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+		User user = this.userRepository.findByUsername(username)
+				.orElseThrow(() -> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
+
+		Page<Feed> paged = this.feedRepository.findByUser(user,
+				PageRequest.of(pageNum, pageSize, Sort.by("feedId").descending()));
+
+		return new PageResponse<Feed>(paged);
+	}
+
 }
